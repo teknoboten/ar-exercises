bbf1fda3cee60160acb8b61ed5fd598c8dff6c1a
serra <serra@modemgirl.ca>
2022-06-29 11:21:08 -0700

    finish exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,29 @@
 require_relative '../setup'
 
 puts "Exercise 1"
-puts "----------"
+puts "----------\n"
 
 # Your code goes below here ...
+
+burnaby = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true,
+  )
+
+richmond = Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true,
+  )
+
+gastown = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false,
+  )
+
+p "total stores: #{Store.all.count}"
