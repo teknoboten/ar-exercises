dc66b129ea2c775faef94d43f19ad7c19539c351
serra <serra@modemgirl.ca>
2022-06-29 11:30:47 -0700

    finish exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+p "@store1's name: #{@store1.name}"
+p "@store2's name: #{@store2.name}"
+
+
+
+
+kits = Store.find_by(id: 1)
+kits.name = "Kitsilano"
+
+p kits.name
